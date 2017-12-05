2f8a69e28979c27e5f2903eb15ea67c78dd4e4da
nterrill95 <31521354+nterrill95@users.noreply.github.com>
2017-12-05 14:51:40 -0600

    Created Main screen layout

--- PhotoEditor/app/src/main/java/com/example/nterrill/photoeditor/MainActivity.java
+++ PhotoEditor/app/src/main/java/com/example/nterrill/photoeditor/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.nterrill.photoeditor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
