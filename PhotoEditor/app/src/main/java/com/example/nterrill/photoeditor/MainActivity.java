2c18f6b3f972008716b8e891a5cda936d70fdd00
nterrill95 <31521354+nterrill95@users.noreply.github.com>
2017-12-05 15:01:25 -0600

    updated app manifest, and created onClick listeners

--- PhotoEditor/app/src/main/java/com/example/nterrill/photoeditor/MainActivity.java
+++ PhotoEditor/app/src/main/java/com/example/nterrill/photoeditor/MainActivity.java
@@ -2,16 +2,35 @@ package com.example.nterrill.photoeditor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button
+    Button mCameraButton;
+    Button mGalleryButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraButton = (Button)findViewById(R.id.CameraButton);
+        mGalleryButton = (Button)findViewById(R.id.GalleryButton);
+
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        mGalleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
